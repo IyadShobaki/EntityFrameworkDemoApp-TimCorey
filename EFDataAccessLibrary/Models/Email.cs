3185592f13b90c7aa8a3fae75650e3bde2aba566
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-07-19 21:22:14 -0400

    Added Fields Validation
    Added models fields validations and updata the database

--- EFDataAccessLibrary/Models/Email.cs
+++ EFDataAccessLibrary/Models/Email.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace EFDataAccessLibrary.Models
@@ -7,6 +9,8 @@ namespace EFDataAccessLibrary.Models
     public class Email
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(200)]
         public string EmailAddress { get; set; }
 
     }
